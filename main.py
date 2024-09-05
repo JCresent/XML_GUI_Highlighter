bd3e20199925d33ca3e2e2fd51d4ee432104f327
Justin Cresent <JCresent@hotmail.com>
2024-09-05 12:08:52 -0400

    Check for cmd line args

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import sys
 import os
 
+def main():
+    #Check to make sure correct amount of args were passed 
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <path_to_dir>")
+        exit(1)
+
+    
+
+
+if __name__ == "__main__":
+    main()  
