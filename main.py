1efb51eb701e3850277bd9a8d0002f4c478fdc6e
Justin Cresent <JCresent@hotmail.com>
2024-09-05 15:38:37 -0400

    Added parsing and fixed bug in apalon.ringtone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
 import os
+import xml.etree.ElementTree as ET
+import PIL
+
 
 def main():
     #Check to make sure correct amount of args were passed 
@@ -13,8 +16,19 @@ def main():
         if file.endswith(".xml"):
             filenames.append(file)
     #print(filenames)
-    
 
+    #Parsing each file for the xml tree and then marking each element and highlighting the PNG files 
+    # Output the annotated PNGs are stored in an Annotated_PNGs directory
+    for file in filenames:
+
+        try:
+            #Trying to grab the tree of the xml file
+            tree = ET.parse(pathDir + file)
+        except:
+            print("Error parsing file: " + file)
+            continue
+        root = tree.getroot()
+    
 
 if __name__ == "__main__":
     main()  
